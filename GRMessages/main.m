9426e9ec402a8f6a03f88da7bbed5e43ddf331c1
woodjobber <woodjobber@outlook.com>
2015-12-30 09:54:25 +0800

    Initial Commit

---
+++ GRMessages/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GRMessages
+//
+//  Created by chengbin on 15/12/30.
+//  Copyright © 2015年 chengbin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
